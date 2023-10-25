a1af25a550d20423fd9b9864be402c02c36f771e
Bea Mungcal <beamungcal@hotmail.com>
2023-10-25 01:07:34 -0400

    Refactor BookCount component

--- components/BookCount.js
+++ components/BookCount.js
@@ -1,22 +1,18 @@
 import React from 'react'
 import { View, Text  } from 'react-native'
 
-
-export default class BookCount extends React.Component {
-
-  render() {
-    return (
-      <View
-        style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent:'center'
-        }}>
-        <Text style={{fontSize: 20}}>
-          {this.props.title}
-        </Text>
-        <Text>{this.props.count}</Text>
-      </View>
-    )
-  }
-}
+const BookCount = ({ title, count }) =>
+  (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent:'center'
+      }}>
+      <Text style={{fontSize: 20}}>
+        {title}
+      </Text>
+      <Text>{count}</Text>
+    </View>
+  )
+export default BookCount;
